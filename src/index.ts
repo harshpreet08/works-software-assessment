2c7ae0c41bbbcf43ecf0aaa6cef41f0e2b7d16c8
Harshpreet Singh <96812198+harshpreet08@users.noreply.github.com>
2024-04-17 13:45:38 -0300

    Removed redundant comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectToMongoDB } from "./db";
+import cors from "cors";
 import noteRoutes from "./notesRoutes"
 
 require('dotenv').config();
@@ -9,6 +10,7 @@ const port = 3000;
 
 connectToMongoDB();
 
+app.use(cors());
 app.use("/notes", noteRoutes);
 
 app.listen(port, () => {
